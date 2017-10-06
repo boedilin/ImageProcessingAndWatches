626b0dd2be1057cf4303d3599218501cc535475f
bossival <bossival@students.zhaw.ch>
2017-10-06 14:19:41 +0200

    added new video material in np format for x and y direction

--- code/experiments/motionVectorArray.py
+++ code/experiments/motionVectorArray.py
@@ -6,10 +6,11 @@ with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     with picamera.array.PiMotionArray(camera, size=(320, 240)) as output:
         camera.start_recording(
-            '/dev/null', format='h264', motion_output=output)
-        camera.wait_recording(10)
+            '/dev/null', format='h264', motion_output=output,
+            resize=(320, 240))
+        camera.wait_recording(30)
         camera.stop_recording()
         print('Captured %d frames' % output.array.shape[0])
         print('Frames are %dx%d blocks big' % (
             output.array.shape[2], output.array.shape[1]))
-        np.save("test.npy", output.array)
+        np.save("tttest.npy", output.array)
---
+++ code/experiments/pencilTest.py
@@ -0,0 +1,37 @@
+"""
+=================
+An animated image
+=================
+
+This example demonstrates how to animate an image.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig = plt.figure()
+data = np.load('testx.npy')
+counter = 0
+
+print('Captured %d frames' % data.shape[0])
+print(data[0]["sad"])
+
+def f():
+    global counter
+    counter += 1
+    if (data.shape[0]-1) != counter:
+        return data[counter]["sad"]
+    else:
+        ani.event_source.stop()
+
+
+# imshow(array)
+im = plt.imshow(f(), animated=True)
+
+
+def updatefig(*args):
+    im.set_array(f())
+    return im,
+
+ani = animation.FuncAnimation(fig, updatefig, interval=20, blit=True)
+plt.show()
