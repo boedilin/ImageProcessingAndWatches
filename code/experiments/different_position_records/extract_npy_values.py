82496870c675ffbc765deec5e8d0b8e847c6677a
bossival <bossival@students.zhaw.ch>
2017-11-14 17:11:01 +0100

    added code to calculate the frequency

--- code/experiments/different_position_records/extract_npy_values.py
+++ code/experiments/different_position_records/extract_npy_values.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-directory="down/80min"
+directory="up/80min"
 data=np.load(directory+"/motion_vectors_80min_black_white.npy")
-with open(directory+"/timestamps_80min_black_white.txt", "r") as ins:
+with open(directory+"/timestamps_40min_black_white.txt", "r") as ins:
     array = []
     for line in ins:
         array.append(line)
@@ -12,7 +12,7 @@ sadValues = open(directory+"/sadValues.txt", "a")
 timestamps = open(directory+"/timestamps.txt", "a")
 
 for index in range(len(array)):
-    if "None" not in array[index]:
+    if ("None" not in array[index]) & (data[index][0] != 0) :
         xValues.write(str(data[index][0])+"\n")
         yValues.write(str(data[index][1])+"\n")
         sadValues.write(str(data[index][2])+"\n")
