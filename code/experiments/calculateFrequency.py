785e6d83def2a0fb9c15690edeb4e879f665d046
Linda Boedi <boedilin@students.zhaw.ch>
2017-11-15 10:40:04 +0100

    changed file content to english

--- code/experiments/calculateFrequency.py
+++ code/experiments/calculateFrequency.py
@@ -2,58 +2,63 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import argrelmin
+from scipy.signal import argrelmax
 
-#timestampsNotCleared = np.loadtxt("different_position_records/down/10min/timestamps_10min_black_white.txt")
-#xySad = np.load("different_position_records/down/10min/motion_vectors_10min_black_white.npy")
-timestamps = np.loadtxt("different_position_records/down/80min/timestamps.txt")
-x = np.loadtxt("different_position_records/down/80min/xValues.txt")
-y = np.loadtxt("different_position_records/down/80min/yValues.txt")
-#x = np.empty(xySad.shape[0])
-#y = np.empty(xySad.shape[0])
-#timestamps = np.empty(xySad.shape[0])
+timestampsNotCleared = np.loadtxt("differents_time_records/48min/timestamps_black_white.txt")
+xySad = np.load("differents_time_records/48min/motion_vectors_black_white.npy")
+movement_sum = np.empty(xySad.shape[0])
+s = np.empty(xySad.shape[0])
+timestamps = np.empty(xySad.shape[0])
 counter = 0
 index = 0
-"""
+alpha = 0.01
+frame_width = 10
+
 for value in range(xySad.shape[0]):
     if xySad[value][0] != 0 and xySad[value][1]!= 0 and xySad[value][2] != 0:
-         x[index] = abs(xySad[value][0])
-         y[index] = abs(xySad[value][1])
+         movement_sum[index] = abs(xySad[value][0]) + abs(xySad[value][1])
+         if index < frame_width:
+             s[index] = movement_sum[index]
+         else:
+             s[index] = alpha * movement_sum[index] + (1 - alpha ) * s[index - 1 ]
+             s[index] = 0
+             for i in range(0,frame_width):
+                 s[index] = s[index] + movement_sum[ index - i ]
+             s[index] =  s[index] / frame_width
          timestamps[index] = timestampsNotCleared[value]
-         index = index + 1
-"""
-x = np.trim_zeros(x)
-y = np.trim_zeros(y)
+         index = index + 1;
+
+movement_sum = np.trim_zeros(movement_sum)
+s = np.trim_zeros(s)
 timestamps = np.trim_zeros(timestamps)
-xmin = argrelmin(x)[0]
-ymin = argrelmin(y)[0]
+
+mean_movement = np.mean(movement_sum)
+
+movement_sum_min = argrelmax(movement_sum)[0]
+smin = argrelmax(s)[0]
 
 firsttimestamp = 0
 lasttimestamp = 0
+plottimestamp = np.empty(smin.size)
+smin_plot = np.empty(smin.size)
+upper_limit_minima = mean_movement + 295
+last_index = smin[0]
 
           
-for min_index in xmin:
-    y_check = 0
-    check_min_in_ymin = False
-    if np.argwhere(ymin == min_index).size != 0:
-        check_min_in_ymin = True
-        y_check = y[min_index]
-    elif np.argwhere(ymin == (min_index + 1)).size != 0:
-        check_min_in_ymin = True
-        y_check = y[min_index+1]
-    elif np.argwhere(ymin == (min_index -1)).size != 0:
-        check_min_in_ymin = True
-        y_check = y[min_index-1]
-    if x[min_index] < 1700 and check_min_in_ymin and y_check < 1700:
+for min_index in smin:
+    if s[min_index] > upper_limit_minima and min_index - last_index > 3:
         counter += 1
+        plottimestamp[counter] = timestamps[ min_index ]
+        smin_plot[counter] = s[min_index]
         if firsttimestamp == 0:
             firsttimestamp = timestamps[ min_index ]
         lasttimestamp = timestamps[ min_index ] 
+        last_index = min_index
    
 print(counter)
-print(timestamps.size)
-x.tofile("differents_time_records/10sec/2_record/x.txt", sep="\n")
-xmin.tofile("differents_time_records/10sec/2_record/xmin.txt", sep="\n")
+print("mean", mean_movement)
+print(lasttimestamp)
+smin.tofile("differents_time_records/10sec/2_record/smin.txt", sep="\n")
 
 duration = ( lasttimestamp - firsttimestamp - 1 ) / 1000 / 1000
 print(duration)
@@ -62,8 +67,9 @@ frequencyXY = 1 / (duration / counter * 2)
 print(frequencyXY)
 
 
-plt.plot(timestamps, x, 'b-', label='sum of x-factor of motion per frame')
-plt.plot(timestamps, y, 'r-', label='sum of y-factor of motion per frame')
+plt.plot(timestamps, s, 'b-', label='sum of x-factor of motion per frame')
+#plt.plot(timestamps, y, 'r*', label='sum of y-factor of motion per frame')
+plt.plot(plottimestamp, smin_plot, 'g*', label='sum of xmin-factor of motion per frame')
  
 plt.grid(b=True, which='both', color='0.65',linestyle='-')
 plt.xlabel('x')
