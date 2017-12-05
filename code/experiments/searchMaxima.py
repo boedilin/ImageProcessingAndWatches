cd4427190da5ad5088135b9f5a4d7a29f56e9d5c
Linda Boedi <boedilin@students.zhaw.ch>
2017-12-05 14:07:03 +0100

    added calculation of Minima with tic toc and search maxima for sad values

---
+++ code/experiments/searchMaxima.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 13:16:42 2017
+
+@author: Linda
+"""
+
+# pylint: disable=invalid-name
+import numpy as np
+import math
+
+frames_per_second = 90
+one_second = 1
+watch_hertz = 3
+watch_hertz_half_period = watch_hertz * 2
+half_period_duration_in_sec = one_second / watch_hertz_half_period
+resolution_grid_in_sec = one_second / frames_per_second
+step_size = math.ceil(half_period_duration_in_sec / resolution_grid_in_sec)
+beginning_led_on = 100
+lower_limit_start_maximum = 500
+
+directory = "records_from_led_board/sadValues/5sec/"
+sadValues = np.loadtxt(directory+"sadValues.txt")
+timestamps = np.loadtxt(directory+"timestamps.txt")
+
+def is_maxima_search_window_5(array, index):
+    left_right_increase = 2
+    if index+left_right_increase < len(array):
+        search_window = np.copy(array[index-left_right_increase:index+left_right_increase])
+        array_to_find_index = np.copy(array[index-left_right_increase:index+left_right_increase])
+        maxima = 0
+        for i in search_window:
+            if i > beginning_led_on:
+                maxima = i
+                break
+        index_in_search_window = np.where(array_to_find_index == maxima)[0][0]
+        return index + (index_in_search_window - left_right_increase)
+
+def find_start_maxima(array):
+    first_30_x_values = np.copy(array[:30])
+    for i in first_30_x_values:
+            if i > lower_limit_start_maximum:
+                return np.where(array[:30] == i)[0][0]
+
+durations_of_half_periods_in_microsec = []
+search_window_increase = 2
+maxima = []
+start_index = find_start_maxima(sadValues)
+maximum = start_index
+start_timestamp = timestamps[start_index]
+while (maximum + step_size + search_window_increase) <= len(sadValues):
+    next_maximum = is_maxima_search_window_5(sadValues, maximum + step_size)
+    maxima.append(next_maximum)
+    maximum = next_maximum
+    durations_of_half_periods_in_microsec.append((timestamps[maximum] - start_timestamp) / len(maxima))
+
+end_timestamp = timestamps[maximum]
+duration_of_half_period_in_microsec = (end_timestamp - start_timestamp) / len(maxima)
+
+for value in range(len(durations_of_half_periods_in_microsec)):
+    durations_of_half_periods_in_microsec[value] = abs(durations_of_half_periods_in_microsec[value]-166666)
+
+durations_of_half_periods_in_microsec.sort()
+
+print("smallest aberration found during calculation: ", durations_of_half_periods_in_microsec[0])
+print("start timestamp", start_timestamp)
+print("end timestamp", end_timestamp)
+print("number of found minimas", len(maxima))
+print("length of half period over all: ", duration_of_half_period_in_microsec)
+print("variation per day: ", ((duration_of_half_period_in_microsec/1000000*21600)-3600)*24)
---
+++ code/experiments/searchMinima_tic_toc.py
@@ -0,0 +1,82 @@
+
+# pylint: disable=invalid-name
+import numpy as np
+import math
+
+frames_per_second = 90
+one_second = 1
+watch_hertz = 3
+watch_hertz_half_period = watch_hertz * 2
+half_period_duration_in_sec = one_second / watch_hertz_half_period
+resolution_grid_in_sec = one_second / frames_per_second
+step_size = math.ceil(half_period_duration_in_sec / resolution_grid_in_sec)
+
+directory = "different_position_records/up/40min/"
+xValues = np.loadtxt(directory+"xValues.txt")
+timestamps = np.loadtxt(directory+"timestamps.txt")
+
+def is_minima_search_window_5(array, index):
+    left_right_increase = 2
+    if index+left_right_increase < len(array):
+        search_window = np.copy(array[index-left_right_increase:index+left_right_increase])
+        array_to_find_index = np.copy(array[index-left_right_increase:index+left_right_increase])
+        search_window.sort()
+        minima = search_window[0]
+        index_in_search_window = np.where(array_to_find_index == minima)[0][0]
+        return index + (index_in_search_window - left_right_increase)
+
+def find_start_minima(array):
+    """
+    returns the index of the lowest value from first 20 values. this must be a stillstand.
+    """
+    first_50_x_values = np.copy(array[:20])
+    first_50_x_values.sort()
+    if first_50_x_values[0] == 0:
+        return np.where(array[:20] == first_50_x_values[1])[0][0]
+    else :
+        return np.where(array[:20] == first_50_x_values[0])[0][0]
+
+durations_of_period_in_microsec = []
+durations_of_period_in_microsec_tic = []
+durations_of_period_in_microsec_toc = []
+search_window_increase = 2
+minima_tic = []
+minima_toc = []
+start_index = find_start_minima(xValues)
+minima = start_index
+switch_to_tic = False
+counter_tic = 0
+counter_toc = 0
+start_timestamp = timestamps[start_index]
+while (minima + step_size + search_window_increase) <= len(xValues):
+    next_minima = is_minima_search_window_5(xValues, minima + step_size)
+    if switch_to_tic:
+        minima_tic.append(next_minima)
+        switch_to_tic = False
+        counter_tic = counter_tic + 1
+        if counter_tic % 2 == 0:
+            durations_of_period_in_microsec_tic.append((timestamps[minima_tic[-1]] - timestamps[minima_tic[-2]]))
+    else:
+        minima_toc.append(next_minima)
+        switch_to_tic = True
+        counter_toc = counter_toc + 1
+        if counter_toc % 2 == 0:
+            durations_of_period_in_microsec_toc.append((timestamps[minima_toc[-1]] - timestamps[minima_toc[-2]]))
+    minima = next_minima
+    if len(durations_of_period_in_microsec_tic) > 0 and len(durations_of_period_in_microsec_toc) > 0:
+        durations_of_period_in_microsec.append((durations_of_period_in_microsec_tic[-1]+durations_of_period_in_microsec_toc[-1])/2)
+
+end_timestamp = timestamps[minima]
+duration_of_period_in_microsec = np.sum(durations_of_period_in_microsec) / len(durations_of_period_in_microsec)
+
+for value in range(len(durations_of_period_in_microsec)):
+    durations_of_period_in_microsec[value] = abs(durations_of_period_in_microsec[value]-166666)
+
+durations_of_period_in_microsec.sort()
+
+print("smallest aberration found during calculation: ", durations_of_period_in_microsec[0])
+print("start timestamp", start_timestamp)
+print("end timestamp", end_timestamp)
+print("number of found minimas", len(minima_tic)+len(minima_toc))
+print("length of half period over all: ", duration_of_period_in_microsec/2)
+print("variation per day: ", ((duration_of_period_in_microsec/2/1000000*21600)-3600)*24)
